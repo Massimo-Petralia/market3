2f69e89f321696de8dc0b0324c6bde6b05ec9d42
Massimo <massipetra@hotmail.it>
2024-08-02 12:30:54 +0200

    feat:(images-preview, dot-indicator, form-product): basic components for select and show images preview defined

--- app/features/product/form-product/form-product-view.tsx
+++ app/features/product/form-product/form-product-view.tsx
@@ -1,8 +1,9 @@
 import {View} from 'react-native';
-import {Text, ActivityIndicator, TextInput} from 'react-native-paper';
+import {Text, ActivityIndicator, TextInput, Button} from 'react-native-paper';
 import {Currency, LoadingState, Product} from '../../../../models/models';
-import { useEffect, useState } from 'react';
-import { DefaultProduct } from '../../../../models/default-values';
+import {useEffect, useState} from 'react';
+import {DefaultProduct} from '../../../../models/default-values';
+import {ImagesPreview} from '../../../components/images-preview';
 
 export const FormProduct = ({
   onCreateProduct,
@@ -13,32 +14,32 @@ export const FormProduct = ({
   loadingState: LoadingState;
   product: Product;
 }) => {
-    const [formProduct, setFormProduct] = useState<Product>(DefaultProduct)
+  const [formProduct, setFormProduct] = useState<Product>(DefaultProduct);
 
-    const updateFormProduct = (key: keyof   Product, value: string|string[]) => {
-        setFormProduct(previuosValue => ({...previuosValue, [key]: value}))
-    }
+  const updateFormProduct = (key: keyof Product, value: string | string[]) => {
+    setFormProduct(previuosValue => ({...previuosValue, [key]: value}));
+  };
 
-    const handleNameChanges = (name: string) => {
-        updateFormProduct('name', name)
-    }
-    const handleDescriptionChanges = (description:string) => {
-        updateFormProduct('description', description)
-    }
-    const handlePriceChanges = (price: string) => {
-        updateFormProduct('price', price)
-    }
-    const handleCurrencyChanges = (currency: Currency)=> {
-        updateFormProduct('currency', currency)
-    }
+  const handleNameChanges = (name: string) => {
+    updateFormProduct('name', name);
+  };
+  const handleDescriptionChanges = (description: string) => {
+    updateFormProduct('description', description);
+  };
+  const handlePriceChanges = (price: string) => {
+    updateFormProduct('price', price);
+  };
+  const handleCurrencyChanges = (currency: Currency) => {
+    updateFormProduct('currency', currency);
+  };
 
-    const handleImagesChanges = (images: string[])=>{
-        updateFormProduct('images', images)
-    }
+  const handleImagesChanges = (images: string[]) => {
+    updateFormProduct('images', images);
+  };
 
-useEffect(()=> {
-    setFormProduct(product)
-},[product])
+  useEffect(() => {
+    setFormProduct(product);
+  }, [product]);
 
   if (loadingState === 'loading') {
     return (
@@ -51,6 +52,13 @@ useEffect(()=> {
     <View>
       <View id="form-product">
         <Text>Form product work !</Text>
+        <ImagesPreview handleImagesChanges={handleImagesChanges} />
+        <Button
+          style={{marginHorizontal: 20}}
+          mode="contained"
+          onPress={() => onCreateProduct(formProduct)}>
+          Save
+        </Button>
       </View>
     </View>
   );
