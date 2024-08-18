2e1ea9a61f0a19fb9489367bbd9bc651846dc896
Massimo <massipetra@hotmail.it>
2024-08-18 16:01:08 +0200

    feat(user-profile): replace buttons with FAB.Group

--- app/features/user/user-profile/user-profile-view.tsx
+++ app/features/user/user-profile/user-profile-view.tsx
@@ -1,10 +1,20 @@
 import {View, StyleSheet, Pressable, ScrollView} from 'react-native';
 import {User} from '../../../../models/models';
-import {Button, Card, useTheme, List, Text, Divider} from 'react-native-paper';
+import {
+  Button,
+  Card,
+  useTheme,
+  List,
+  Text,
+  Divider,
+  Portal,
+  FAB,
+} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
 import Routes from '../../../navigation/routes';
 import {UserStackNavigationProp} from '../../../navigation/navigation-types';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {useState} from 'react';
 
 export const UserProfile = ({
   onLogout,
@@ -13,95 +23,105 @@ export const UserProfile = ({
   onLogout: () => void;
   userDetail: User;
 }) => {
+  const [fabGroup, setFabGroup] = useState({open: false});
   const navigation = useNavigation<UserStackNavigationProp>();
   const theme = useTheme();
   const defaultAvatar = require('../../../assets/images/user.png');
+  const closeFabGroup = () => {
+    setFabGroup({open: false});
+  };
   return (
-    <ScrollView>
-      <View id="user-detail">
-        <Card style={{marginVertical: 5}}>
-          <Pressable>
-            <Card.Cover
-              style={[style.pressable, {borderColor: theme.colors.primary}]}
-              source={
-                !userDetail.avatar.uri
-                  ? defaultAvatar
-                  : {uri: userDetail.avatar.uri}
-              }
+    <View>
+      <ScrollView style={{marginHorizontal: 20}}>
+        <View id="user-detail">
+          <Card style={{marginVertical: 5}}>
+            <Pressable>
+              <Card.Cover
+                style={[style.pressable, {borderColor: theme.colors.primary}]}
+                source={
+                  !userDetail.avatar.uri
+                    ? defaultAvatar
+                    : {uri: userDetail.avatar.uri}
+                }
+              />
+            </Pressable>
+            <Divider horizontalInset />
+            <Card.Title
+              title="Profile details"
+              titleStyle={{paddingLeft: 16}}
             />
-          </Pressable>
-          <Divider horizontalInset/>
-          <Card.Title title="Profile details" titleStyle={{paddingLeft: 16}} />
-          <Card.Content>
-            <List.Item
-              contentStyle={style.listItem}
-              title="Name: "
-              description={userDetail.name}
-            />
-            <List.Item
-              contentStyle={style.listItem}
-              title="E-mail: "
-              description={userDetail.email}
-            />
-          </Card.Content>
-          <Card.Actions>
-            <Button
-              style={{alignSelf: 'center' , marginRight: 16}}
-              mode="contained"
-              onPress={() => onLogout()}>
-              Logout
-            </Button>
-          </Card.Actions>
-        </Card>
-        <Card style={{marginVertical: 5}}>
-          <Card.Title
-            title="Address"
-            titleStyle={{paddingLeft: 16}}
-            right={props => (
-              <Button
-                mode="contained"
-              style={{marginRight: 23}}
-                onPress={() =>
-                  navigation.navigate(Routes.MainTabs.UserStack.Address)
-                }>
-                <FontAwesome size={20} name="edit" />
-                {' '}Edit
-              </Button>
-            )}
-          />
-
-          {userDetail.address.address ? (
             <Card.Content>
               <List.Item
-                style={style.listItem}
-                title="Address: "
-                description={userDetail.address.address}
+                contentStyle={style.listItem}
+                title="Name: "
+                description={userDetail.name}
               />
               <List.Item
-                style={style.listItem}
-                title="City: "
-                description={userDetail.address.city}
-              />
-              <List.Item
-                style={style.listItem}
-                title="State: "
-                description={userDetail.address.state}
-              />
-              <List.Item
-                style={style.listItem}
-                title="Country: "
-                description={userDetail.address.country}
-              />
-              <List.Item
-                style={style.listItem}
-                title="Zip code: "
-                description={userDetail.address.zipcode}
+                contentStyle={style.listItem}
+                title="E-mail: "
+                description={userDetail.email}
               />
             </Card.Content>
-          ) : null}
-        </Card>
-      </View>
-    </ScrollView>
+            <Divider horizontalInset />
+
+            <Card.Title title="Address" titleStyle={{paddingLeft: 16}} />
+
+            {userDetail.address.address ? (
+              <Card.Content>
+                <List.Item
+                  style={style.listItem}
+                  title="Address: "
+                  description={userDetail.address.address}
+                />
+                <List.Item
+                  style={style.listItem}
+                  title="City: "
+                  description={userDetail.address.city}
+                />
+                <List.Item
+                  style={style.listItem}
+                  title="State: "
+                  description={userDetail.address.state}
+                />
+                <List.Item
+                  style={style.listItem}
+                  title="Country: "
+                  description={userDetail.address.country}
+                />
+                <List.Item
+                  style={style.listItem}
+                  title="Zip code: "
+                  description={userDetail.address.zipcode}
+                />
+              </Card.Content>
+            ) : null}
+          </Card>
+        </View>
+      </ScrollView>
+      <Portal>
+        <FAB.Group
+          style={{marginBottom: 80}}
+          open={fabGroup.open}
+          visible
+          icon={!fabGroup.open ? 'menu':'menu-open'}
+          onStateChange={({open}) => setFabGroup({open})}
+          actions={[
+            {
+              icon: 'logout',
+              onPress: () => onLogout(),
+            },
+            {
+              icon: 'pencil-box',
+              label: 'Address',
+              onPress: () => {
+                closeFabGroup();
+                navigation.navigate(Routes.MainTabs.UserStack.Address);
+              },
+            },
+          ]}
+        />
+      </Portal>
+    </View>
   );
 };
 
